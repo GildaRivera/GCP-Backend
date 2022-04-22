43f04c424e4e4805b765a7ecf23ebf74f0d4bab3
gildarivera <gilda.rivera@telus.com>
2022-04-22 17:23:15 -0600

    feat: First commit

---
+++ src/routes/routes.js
@@ -0,0 +1,32 @@
+module.exports = routes => {
+    const user= require("../User/controllers/user.controllers")
+    const picture = require("../Picture/controllers/picture.controllers")
+    var router = require("express").Router();
+    // USER
+    // Create a new user
+    // router.post("/user", user.createUser);
+    // // Retrieve all user
+    // router.get("/user", user.getAllUsers);
+    // // Retrieve a single user with id
+    // router.get("/user/:id", user.getUser);
+    // // Update a user with id
+    // router.put("/user/:id", user.updateUser);
+    // // Delete a user with id
+    // router.delete("/user/:id", user.deleteUser);
+    // // Login user
+    // router.post("/login", user.loginUser);
+
+
+    
+    // // Picture
+    // // Create a new picture
+    // router.post("/picture", user.createPicture);
+    // // Retrieve all pictures
+    // router.get("/picture", user.getAllPictures);
+
+
+
+
+
+    routes.use('/api', router);
+  };
