71a275624a0420afd08752b2155029f2eda11e40
gildarivera <gilda.rivera@telus.com>
2022-04-24 16:32:17 -0600

    feat: adding user controllers

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,17 +4,17 @@ module.exports = routes => {
     var router = require("express").Router();
     // USER
     // Create a new user
-    // router.post("/user", user.createUser);
-    // // Retrieve all user
-    // router.get("/user", user.getAllUsers);
-    // // Retrieve a single user with id
-    // router.get("/user/:id", user.getUser);
-    // // Update a user with id
-    // router.put("/user/:id", user.updateUser);
-    // // Delete a user with id
-    // router.delete("/user/:id", user.deleteUser);
-    // // Login user
-    // router.post("/login", user.loginUser);
+    router.post("/user", user.createUser);
+    // Retrieve all user
+    router.get("/user", user.getAllUsers);
+    // Retrieve a single user with id
+    router.get("/user/:id", user.getUser);
+    // Update a user with id
+    router.put("/user/:id", user.updateUser);
+    // Delete a user with id
+    router.delete("/user/:id", user.deleteUser);
+    // Login user
+    router.post("/login", user.loginUser);
 
 
     
